32d6c9645363b0c05d829c3944c561723469b87c
RahulPandit7 <prajapati98162@gmail.com>
2026-06-16 14:03:53 +0545

    feat: initialize Prisma client with pg adapter for database connectivity

--- server/dist/config/prisma.js
+++ server/dist/config/prisma.js
@@ -3,8 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
 const adapter_pg_1 = require("@prisma/adapter-pg");
+const client_1 = require("@prisma/client");
 const pg_1 = __importDefault(require("pg"));
 const connectionString = process.env.DATABASE_URL;
 const pool = new pg_1.default.Pool({ connectionString });
